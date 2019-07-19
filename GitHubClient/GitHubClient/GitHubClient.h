05117bf2448c6c44a8bdd8bb68ed8ac556c1744e
hicka04 <hicka04@gmail.com>
2019-07-19 10:36:41 +0900

    add project

---
+++ GitHubClient/GitHubClient/GitHubClient.h
@@ -0,0 +1,19 @@
+//
+//  GitHubClient.h
+//  GitHubClient
+//
+//  Created by hicka04 on 2019/07/19.
+//  Copyright © 2019 hicka04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GitHubClient.
+FOUNDATION_EXPORT double GitHubClientVersionNumber;
+
+//! Project version string for GitHubClient.
+FOUNDATION_EXPORT const unsigned char GitHubClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitHubClient/PublicHeader.h>
+
+
